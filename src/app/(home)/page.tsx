a5436c67adeb1d998143ca9a028f5bb1bf09c65f
Yash <yashskolambekar2015@gmail.com>
2025-01-04 23:24:49 +0530

    made hero responsive

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Hero from "./Hero";
+import LatestDrops from "./LatestDrops";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      {/* <LatestDrops /> */}
     </div>
   );
 }
